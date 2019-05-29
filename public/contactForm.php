5e20c16b9c3021ef2868f5cc24cefe3c6f8e8220
Robbe <26785@ma-web.nl>
2019-05-29 10:08:41 +0200

    Added a whole bunch of files
    Added necessary files.

--- public/contactForm.php
+++
@@ -1,8 +0,0 @@
-<form class="contact">
-    <h2 class="formTitle">Contact</h2>
-    <input type="email" name="email" placeholder="Email" class="emailInput" required>
-    <input type="text" name="firstName" placeholder="first name" class="FNinput" required>
-    <input type="text" name="lastName" placeholder="last name" class="LNinput" required>
-    <input type="text" name="subject" placeholder="Subject" class="subjectInput" required>
-    <input type="textfield" name="message" placeholder="Message..." class="msgInput" required>
-</form>
