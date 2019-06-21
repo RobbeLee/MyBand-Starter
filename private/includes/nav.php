343eedd10433831bb6e664e24cf611aee0b78d96
Robbe <26785@ma-web.nl>
2019-06-21 14:21:14 +0200

    Responsive?
    Home page and nav are now responsive.

--- private/includes/nav.php
+++ private/includes/nav.php
@@ -1,14 +1,16 @@
 <link rel="stylesheet" href="<?php echo url('/css/nav.css')?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<nav class="nav">
-    <div class="left-container">
-        <a href="/MyBand-Starter/public/" class="left-container__brand" title="The Chain">The Chain</a>
-    </div>
+<nav class="nav" id="nav">
+    <a href="/MyBand-Starter/public/" class="left__a">The Chain</a>
     <div class="right-container">
-        <a href="/MyBand-Starter/public/" class="right-container__a">Home</a>
-        <a href="/MyBand-Starter/public/story" class="right-container__a">Story</a>
         <a href="/MyBand-Starter/public/agenda" class="right-container__a">Agenda</a>
         <a href="/MyBand-Starter/public/contact" class="right-container__a">Contact</a>
         <a href="/MyBand-Starter/public/search" class="right-container__a">Search</a>
+        <a href="/MyBand-Starter/public/story" class="right-container__a">Story</a>
     </div>
-</nav>
+    <a href="javascript:void(0);" class="icon" onclick="responsive()">
+        <i class="fa fa-bars"></i>
+    </a>
+</nav> 
+<script src="js/nav.js"></script>
