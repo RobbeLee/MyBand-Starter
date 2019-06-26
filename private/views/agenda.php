fad200dae45381434ac3fb9c0e07996788d3c1db
Robbe <26785@ma-web.nl>
2019-06-26 09:27:03 +0200

    Added "php" and some css
    Added php so there's an actual agenda and some css so the google agenda is visible.

--- private/views/agenda.php
+++ private/views/agenda.php
@@ -9,7 +9,7 @@ Agenda
 <?php $this->stop('css')?>
 
 <div class="wrapper">
-    <? foreach ($Agenda as $agenda): ?>
+    <?php foreach ($Agenda as $agenda): ?>
         <div class="text">
             <h2 class="text__title">
             <?=htmlspecialchars($agenda['CalendarTitle'])?>
@@ -20,7 +20,7 @@ Agenda
         </div>
     <?php endforeach ?>
     <div class="agenda">
-        <iframe src="https://calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=dGhlY2hhaW5vZmFjaGVyb25AZ21haWwuY29t&amp;color=%23039BE5" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        <iframe src="//calendar.google.com/calendar/b/2/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=dGhlY2hhaW5vZmFjaGVyb25AZ21haWwuY29t&amp;color=%23039BE5" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </div>
     <? //foreach ($Agenda as $agenda): ?>
         <div class="times" id="times">
